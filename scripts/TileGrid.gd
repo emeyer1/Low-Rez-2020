b19d473e08d7f36779185d34cb9ca1f9838716d2
Edwin <edwinmeyer47@gmail.com>
2020-08-06 19:24:50 -0400

    passing activated rows to the battle system

--- scripts/TileGrid.gd
+++ scripts/TileGrid.gd
@@ -5,7 +5,7 @@ signal turn_ended(activations)
 var nTiles: Vector2 = Vector2(3, 9)
 var size: int = 7
 var margin: int = 1
-var activation_rows
+var activation_rows = 2
 var tiles = []
 var random = RandomNumberGenerator.new()
 var tile_texture_path = "res://assets/tiles/"
@@ -26,24 +26,28 @@ func _ready():
 		for y in range(0, nTiles.y):
 			tiles[x].append(get_next_tile())
 			set_tile_position(tiles[x][y], x, y)
-			tiles[x][y].set_size(Vector2(size, size))
-			tiles[x][y].connect("button_up", self, "on_button_clicked", [tiles[x][y]])
+			tiles[x][y].button.set_size(Vector2(size, size))
+			tiles[x][y].button.connect("button_up", self, "on_button_clicked", [tiles[x][y]])
 #			tiles[x][y].set_material(tile_material)
-			add_child(tiles[x][y])
+			add_child(tiles[x][y].button)
 
 func get_next_tile():
-	var ret = TextureButton.new()
+	var ret = { "button" : TextureButton.new()}
 	var texture
 	match random.randi_range(0, 3):
 		0:
 			texture = textures["earthblock"]
+			ret.tileType = "earthblock"
 		1:
 			texture = textures["fireblock"]
+			ret.tileType = "fireblock"
 		2:
 			texture = textures["block2"]
+			ret.tileType = "block2"
 		3:
 			texture = textures["block2"]
-	ret.set_normal_texture(texture)
+			ret.tileType = "block2"
+	ret.button.set_normal_texture(texture)
 	return ret
 
 func on_button_clicked(tile):
@@ -73,7 +77,8 @@ func on_button_clicked(tile):
 				moves_remaining -= 1
 				if(moves_remaining == 0):
 					player_turn = false
-					emit_signal("turn_ended", [])
+					var activations = activate_tiles()
+					emit_signal("turn_ended", activations)
 
 func is_tile_in_swap_range(position1, position2):
 	var y = int(abs(position1.y - position2.y))
@@ -93,7 +98,45 @@ func get_textures():
 	dir.list_dir_end()
 
 func set_tile_position(tile, x, y):
-	tile.set_position(Vector2(x * size + margin, y * size + margin))
+	tile.button.set_position(Vector2(x * size + margin, y * size + margin))
+
+func activate_tiles():
+	var activations = []
+	
+	#get x activations
+	for y in range(0, nTiles.y):
+		var last_tile = null
+		var chain = 1
+		for x in range(0, nTiles.x):
+			if tiles[x][y].tileType == last_tile:
+				chain += 1
+			else:
+				if chain >= 3:
+					activations.append({"tileType": last_tile, "x" : x - chain, "y": y, "direction": "x", "length": chain})
+				last_tile = tiles[x][y].tileType
+				chain = 1
+		if chain >= 3:
+			activations.append({"tileType": last_tile, "x" :  nTiles.x - chain, "y":y , "direction": "x", "length": chain})
+		last_tile = null
+	
+	#get y activations
+	for x in range(0, nTiles.x):
+		var last_tile = null
+		var chain = 1
+		for y in range(0, nTiles.y):
+			if tiles[x][y].tileType == last_tile:
+				chain += 1
+			else:
+				if chain >= 3:
+					activations.append({"tileType": last_tile, "x" : x, "y": y - chain, "direction": "y", "length": chain})
+				last_tile = tiles[x][y].tileType
+				chain = 1
+		if chain >= 3:
+			activations.append({"tileType": last_tile, "x" : x, "y": nTiles.y - chain, "direction": "y", "length": chain})
+		last_tile = null
+		
+	print("activations: " + str(activations))
+	return activations
 
 func _on_Main_turn_start():
 	player_turn = true
